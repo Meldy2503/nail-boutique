b80ad6c81e5ab05c3c20fff22ba1845ca4bdeff6
meldy2503 <emelder.charles25@gmail.com>
2022-03-11 06:25:37 +0100

    create service location page and add contents

--- src/components/myAppointments/booking/index.js
+++ src/components/myAppointments/booking/index.js
@@ -4,13 +4,143 @@ import {
   RightContent,
 } from "../../../reuseableComponents/containerStyle";
 import Sidebar from "../../sidebar";
+import { Button } from "../../../reuseableComponents/buttonStyle";
+import { useFormik, yupToFormErrors } from "formik";
+import * as Yup from "yup";
+import {
+  Col1,
+  Title,
+  ServiceContainer,
+  Services,
+  ServiceType,
+  FormContainer,
+  FormContents,
+  InputContainer,
+  Btn,
+} from "./bookingStyle";
+// import { Link } from "react-router-dom";
+import { FaAngleLeft, FaAngleDown, FaAngleRight } from "react-icons/fa";
+import serviceData from "./bookingData";
 
 function Booking() {
+  const [onClick, setOnClick] = React.useState({});
+  const handleClick = (index) => () => {
+    setOnClick((state) => ({
+      ...state,
+      [index]: !state[index],
+    }));
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      options: [],
+    },
+    validationSchema: Yup.object({
+      options: Yup.array()
+        .min(1, "select atleast one option")
+        .required("Required"),
+      // options: Yup.boolean().oneOf([true], "select atleast one option"),
+    }),
+
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
+
   return (
     <ContentContainer>
       <Sidebar />
       <RightContent>
-        <h2>Meldy</h2>
+        <Col1>
+          <Title>
+            <h2>Select Services</h2>
+            <div>
+              <FaAngleLeft />
+              <a href="#">GO BACK</a>
+            </div>
+          </Title>
+          <ServiceContainer>
+            {serviceData.map((items, index) => {
+              return (
+                <Services key={items.id}>
+                  <ServiceType>
+                    <div>
+                      <h3>{items.title}</h3>
+                      <p>{items.text}</p>
+                    </div>
+                    <span onClick={handleClick(index)}>
+                      {onClick[index] ? <FaAngleRight /> : <FaAngleDown />}
+                    </span>
+                  </ServiceType>
+
+                  {onClick[index] && (
+                    <FormContainer onSubmit={formik.handleSubmit}>
+                      <FormContents>
+                        <InputContainer>
+                          <input
+                            type="checkbox"
+                            id="optionA"
+                            name="options"
+                            value={formik.values.options}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                          <label htmlFor="optionA">
+                            <h5>Basic Manicure</h5>
+                            <p>30 mins · ₦5000</p>
+                          </label>
+                          {formik.touched.options && formik.errors.options ? (
+                            <p className="errors">{formik.errors.options}</p>
+                          ) : null}
+                        </InputContainer>
+
+                        <InputContainer>
+                          <input
+                            type="checkbox"
+                            id="optionB"
+                            name="options"
+                            value={formik.values.options}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                          <label htmlFor="optionB">
+                            <h5>Basic Manicure</h5>
+                            <p>30 mins · ₦5000</p>
+                          </label>
+                          {formik.touched.options && formik.errors.options ? (
+                            <p className="errors">{formik.errors.options}</p>
+                          ) : null}
+                        </InputContainer>
+
+                        <InputContainer>
+                          <input
+                            type="checkbox"
+                            id="optionC"
+                            name="options"
+                            value={formik.values.options}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                          />
+                          <label htmlFor="optionC">
+                            <h5>Basic Manicure</h5>
+                            <p>30 mins · ₦5000</p>
+                          </label>
+                          {formik.touched.options && formik.errors.options ? (
+                            <p className="errors">{formik.errors.options}</p>
+                          ) : null}
+                        </InputContainer>
+                      </FormContents>
+                    </FormContainer>
+                  )}
+                </Services>
+              );
+            })}
+          </ServiceContainer>
+          {/* <Btn>
+            <Button />
+          </Btn> */}
+        </Col1>
       </RightContent>
     </ContentContainer>
   );
--- src/components/sidebar/sidebarStyle.js
+++ src/components/sidebar/sidebarStyle.js
@@ -11,15 +11,42 @@ export const StyledUl = styled.ul`
     padding: 1rem 0 0 0;
   }
 
-  .navLink {
-    font-size: 1.6rem;
-    font-weight: 400;
-    color: ${({ theme }) => theme.colors.secondary5};
-    padding: 2rem 3rem;
+  & > li {
+    padding: 1.5rem 0 2.5rem 2rem;
 
-    & > span {
+    @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+      padding: 1.5rem 0 2.5rem 0.5rem;
+    }
+    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+    }
+
+    .navLink {
+      font-size: 1.6rem;
+      font-weight: 400;
+      color: ${({ theme }) => theme.colors.secondary5};
+      padding: 2rem 3rem;
+
+      @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+        padding: 2rem 2.3rem 2rem 2rem;
+      }
+
       @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
-        display: none;
+        padding: 2rem 3rem 2rem 1rem;
+      }
+
+      & > span {
+        @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+          display: none;
+        }
+      }
+    }
+
+    .active {
+      background: linear-gradient(150deg, #141414 85%, #1c1c1c 100%);
+      color: ${({ theme }) => theme.colors.tertiary1};
+
+      @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+        background: transparent;
       }
     }
 
@@ -30,38 +57,7 @@ export const StyledUl = styled.ul`
         margin: 0 auto;
         font-size: 2.5rem;
         display: inline-block;
-        /* margin-bottom: 2rem; */
       }
-    }
-
-    @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
-      padding: 2rem 2.3rem 2rem 2rem;
-      /* text-align: center; */
-    }
-
-    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
-      padding: 2rem 3rem 2rem 1rem;
-      /* text-align: center; */
-    }
-  }
-
-  .active {
-    background: linear-gradient(150deg, #141414 85%, #1c1c1c 100%);
-    color: ${({ theme }) => theme.colors.tertiary1};
-
-    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
-      background: transparent;
-    }
-  }
-
-  & > li {
-    padding: 1.5rem 0 2.5rem 2rem;
-
-    @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
-      padding: 1.5rem 0 2.5rem 0.5rem;
-    }
-    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
-      /* padding: 0; */
     }
   }
 `;
