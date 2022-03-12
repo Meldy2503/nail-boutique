6af1e75e350e3757a24b7bdb3ca55e7a3757f7ba
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-03-12 21:04:14 +0100

    modify code

--- src/components/myAppointments/confirmBooking/confirmBookingStyle.js
+++ src/components/myAppointments/confirmBooking/confirmBookingStyle.js
@@ -11,20 +11,31 @@ export const Location = styled.div`
   background-color: ${({ theme }) => theme.colors.primary3};
   margin-bottom: 2rem;
   border-radius: 0.5rem;
+  width: 56rem;
 
-  width: 50%;
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    width: 75%;
+  }
+  @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+    width: 95%;
+  }
+
   span {
     display: flex;
     justify-content: space-evenly;
     align-items: center;
     padding: 3rem;
+    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+      padding: 1rem;
+    }
   }
   .pin {
     font-size: 3.5rem;
+    width: 20%;
   }
 
   p {
-    /* width: 30rem; */
+    width: 80%;
   }
 `;
 
