512cff10dce9deb455db427f8cdb4f47b25a0232
Egbuchulam Chibuokem <Buokem@Buokems-MacBook-Pro.local>
2022-03-14 12:00:04 +0100

    create success page popup

--- src/components/myAppointmentsPersonal/success/successStyle.js
+++ src/components/myAppointmentsPersonal/success/successStyle.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const SuccessContainer = styled.div`
-  /* position: relative; */
   display: flex;
   justify-content: center;
   align-items: center;
