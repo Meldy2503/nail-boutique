1d2c3cd06f8891aa61d910e8c52796b65063cc7a
meldy2503 <emelder.charles25@gmail.com>
2022-03-14 07:44:39 +0100

    add mediaQuery to booking

--- src/components/myAppointmentsPersonal/booking/bookingStyle.js
+++ src/components/myAppointmentsPersonal/booking/bookingStyle.js
@@ -2,42 +2,59 @@ import styled from "styled-components";
 
 export const BookingStyle = styled.div`
   display: flex;
-  justify-content: center;
+
   align-items: center;
   height: 100%;
+  width: 80%;
+  margin: 0 auto;
+  justify-content: space-between;
+
+  @media screen and (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+  }
 `;
 
 export const Book = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 3rem;
-  /* height: 100%; */
-  width: 70%;
+  height: 30rem;
+  width: 48%;
+  border-radius: 1rem;
+  border: 1px solid #636363;
 
-  & > div {
-    width: 50%;
-    /* height: 60vw; */
-    border-radius: 1rem;
-    border: 1px solid ${({ theme }) => theme.colors.secondary4};
-    /* display: flex; */
+  @media screen and (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+    width: 55%;
 
-    & > div {
-      padding: 15%;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      height: 29rem;
-
-      h4 {
-        width: 9rem;
-        color: ${({ theme }) => theme.colors.secondary1};
-      }
-    }
+    margin-top: 2rem;
   }
 
-  img {
-    width: 30%;
-    max-width: 100%;
+  @media screen and (max-width: 480px) {
+    width: 80%;
+  }
+
+  & > div {
+    padding: 4rem 4rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+
+    @media screen and (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+      padding: 3rem 4rem;
+    }
+
+    h4 {
+      font-size: 2rem;
+      line-height: 1.3;
+      width: 10rem;
+      color: ${({ theme }) => theme.colors.secondary1};
+    }
+
+    img {
+      color: #fff;
+      width: 5rem;
+      height: 5rem;
+      max-width: 100%;
+    }
   }
 `;
