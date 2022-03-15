9f305ee9bf0adba8d48d251ef62be0db421fcec7
meldy2503 <emelder.charles25@gmail.com>
2022-03-15 08:22:39 +0100

    update fontsize

--- src/reuseableComponents/headingStyle.js
+++ src/reuseableComponents/headingStyle.js
@@ -6,19 +6,30 @@ export const HeadingStyle = styled.div`
   margin-bottom: 1rem;
   align-items: center;
 
+  @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
+    padding-top: 1rem;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
+    padding-top: 3rem;
+  }
+
   h1 {
-    font-size: 3rem;
+    font-size: 2.8rem;
     font-style: normal;
     font-weight: 700;
     /* line-height: 1.4; */
 
     @media (max-width: ${({ theme }) => theme.mediaQuery.tablet}) {
-      font-size: 2.8rem;
-      padding-top: 1rem;
+      font-size: 2.5rem;
     }
 
-    @media (max-width: ${({ theme }) => theme.mediaQuery.mobile}) {
-      padding-top: 2rem;
+    @media (max-width: 500px) {
+      font-size: 2.2rem;
+    }
+
+    @media (max-width: 350px) {
+      font-size: 1.9rem;
     }
   }
 `;
